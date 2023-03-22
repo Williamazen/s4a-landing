b6ce4f283025452b9929bac8c0270404f54507c9
Williamazem <william.azen@gmail.com>
2023-03-22 20:02:26 +0000

    fix

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -2,14 +2,14 @@ import { SocialRow, Link } from './StyledSocials'
 import { AiFillInstagram } from 'react-icons/ai';
 import { FaFacebookSquare } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
-import { links } from '../../global';
+import { links, language } from '../../global';
 
 export default function Socials(props) {
     return (
         <SocialRow {...props}>
-            <Link href={links.instagram} target="_blank"> <AiFillInstagram /></Link>
-            <Link href={links.facebook} target="_blank"> <FaFacebookSquare /></Link>
-            <Link href={links.email} target="_blank"> <MdMail /></Link>
+            <Link href={links.instagram} target="_blank" role="Instagram" > <AiFillInstagram /></Link>
+            <Link href={links.facebook} target="_blank" role="Facebook"> <FaFacebookSquare /></Link>
+            <Link href={links.email} target="_blank" role="email"> <MdMail /></Link>
         </SocialRow>
     )
 }
