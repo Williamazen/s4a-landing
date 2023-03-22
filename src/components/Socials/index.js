ca00134d641034902bcb2bcf31cdde9fba833901
Williamazen <42363248+Williamazen@users.noreply.github.com>
2023-03-22 20:10:17 +0000

    Update index.js

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -7,9 +7,9 @@ import { links, language } from '../../global';
 export default function Socials(props) {
     return (
         <SocialRow {...props}>
-            <Link href={links.instagram} target="_blank" role="Instagram" > <AiFillInstagram /></Link>
-            <Link href={links.facebook} target="_blank" role="Facebook"> <FaFacebookSquare /></Link>
-            <Link href={links.email} target="_blank" role="email" > <MdMail /></Link>
+            <Link href={links.instagram} target="_blank" title="Instagram" > <AiFillInstagram /></Link>
+            <Link href={links.facebook} target="_blank" title="Facebook"> <FaFacebookSquare /></Link>
+            <Link href={links.email} target="_blank" title="email" > <MdMail /></Link>
         </SocialRow>
     )
 }
